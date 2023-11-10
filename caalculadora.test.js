7caa447a4f0d8f1748501615c4aa479d4aa12301
Danilo Micael da Silva <1221316556@acd.grupotiradentes.com>
2023-11-10 10:29:50 -0300

    subtrair

--- caalculadora.test.js
+++ caalculadora.test.js
@@ -1,4 +1,4 @@
-const { somar } = require('./calculador')
+const { somar, subtrair } = require('./calculador')
 
 describe('Teste da função somar', () => {
     test('deveria somar dois numeros inteiros', () => {
@@ -9,4 +9,8 @@ describe('Teste da função somar', () => {
     test('Deveria somar dois numeros inteiros negativos', () => {
         expect(somar(-1,-1)).toBe(-2);
     });
+    test('Deveria subutrarir dois numeros inteiros', () => {
+        expect(subtrair(2,1)).toBe(1);
+        expect(subtrair(5,2)).toBe(3);
+    })
 });
--- calculador.js
+++ calculador.js
@@ -1,4 +1,9 @@
 function somar(x,y){
     return x + y;
 }
-module.exports = { somar };
+
+function subtrair(x,y){
+    return x- y;
+}
+module.exports = { somar, subtrair };
+
